3f937cf7b6076705181873f933987a1ae338b639
Abdallah <abdallah@MacBook-Pro-de-Admin.local>
2021-10-06 09:41:18 +0200

    debut projet

---
+++ projet-blog/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Acceuil/>
+  </div>
+</template>
+
+<script>
+import Acceuil from './components/Acceuil.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Acceuil
+  }
+}
+</script>
+
+<style>
+
+</style>
