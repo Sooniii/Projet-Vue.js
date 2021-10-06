c48a15072491d7bc6b7fdb2efa1fdb594b3f38a1
Loic-Gouriou <loicgouriou6@gmail.com>
2021-10-06 13:15:44 +0200

    AJout du fichier listeArticle

---
+++ projet-blog/src/components/listeArticle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <p> {{title}} {{intro}} {{temps}} {{content}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        id: Number,
+        title: String,
+        intro: String,
+        temps: String,
+        content: String,
+    }
+
+}
+</script>
+
+<style>
+
+</style>
